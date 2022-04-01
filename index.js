d3187b6f86c2bdcfcacfc3bef10009cc0c9651f5
Ngan Pham <nganp@email.arizona.edu>
2022-03-31 20:36:39 -0700

    complete 1->5

--- index.js
+++ index.js
@@ -1,46 +1,81 @@
-const express = require('express');
-const {store} = require('./temporarily-store/store');
+const express = require("express");
+const cors = require("cors");
+const { store } = require("./temporarily-store/store");
 
 const application = express();
-const port = process.env.PORT || 4002 ;
+const port = process.env.PORT || 4002;
 
 //middlewares
 application.use(express.json());
+application.use(cors());
 
 //methods
-application.get('/', (request, response) => {
-    response.status(200).json({ done: true, message: 'Welcome to image quiz backend API!' });
+application.get("/", (request, response) => {
+  response
+    .status(200)
+    .json({ done: true, message: "Welcome to image quiz backend API!" });
 });
 
-application.post('/register', (request, response) => {
-    let name = request.body.name;
-    let email = request.body.email;
-    let password = request.body.password;
-    store.addCustomer(name, email, password)
-    response.status(200).json({ done: true, message: 'The customer was added successfully!' });
-    });
-
-application.post('/login', (request, response) => {
-    let email = request.body.email;
-    let password = request.body.password;
-    let result = store.login(email, password);
-    if (result.valid) {
-    response.status(200).json({ done: true, message: 'The customer was added successfully!' });
-    } else {
-        response.status(401).json({ done: false, message: result.message});
-    }
+application.post("/register", (request, response) => {
+  let name = request.body.name;
+  let email = request.body.email;
+  let password = request.body.password;
+  let register = store.addCustomer(name, email, password);
+  if (register.done == false) {
+    response
+      .status(403)
+      .json({ done: true, message: "The customer was not added!" });
+  } else {
+    response
+      .status(200)
+      .json({ done: true, message: "The customer was added successfully!" });
+  }
 });
 
-application.get('/quiz/:id', (request, response) => {
-    let id = request.params.id;
-    let result = store.getQuiz(id);
-    if (result.done) {
-        response.status(200).json({ done: true, result: result.quiz});
-    }  else {
-        response.status(404).json({done: false, message: result.message});
-    }
+application.post("/login", (request, response) => {
+  let email = request.body.email;
+  let password = request.body.password;
+  let result = store.login(email, password);
+  if (result.valid) {
+    response
+      .status(200)
+      .json({ done: true, message: "The customer was added successfully!" });
+  } else {
+    response.status(401).json({ done: false, message: result.message });
+  }
+});
+
+application.get("/quiz/:id", (request, response) => {
+  let id = request.params.id;
+  let result = store.getQuiz(id);
+  if (result.done) {
+    response.status(200).json({ done: true, result: result.quiz });
+  } else {
+    response.status(404).json({ done: false, message: result.message });
+  }
+});
+
+application.get("/flowers", (request, response) => {
+  //let flowers = request.params.flowers;
+  let result = store.getFlower();
+  if (result.done) {
+    response.status(200).json({ done: true, result: result.flowerlist });
+  } else {
+    response.status(404).json({ done: false, message: result.message });
+  }
+});
+
+application.post("/score", (request, response) => {
+  let quizTaker = request.body.quizTaker;
+  let quizName = request.body.quizName;
+  let score = request.body.score;
+  let date = new Date().toISOString().slice(0, 10);
+  let store = store.score(quizTaker, quizName, score, date);
+  if (store.done) {
+    response.status(200).json({ done: true, message: store.message });
+  }
 });
 
 application.listen(port, () => {
-    console.log(`Listening to the port ${port} `);
+  console.log(`Listening to the port ${port} `);
 });
--- temporarily-store/store.js
+++ temporarily-store/store.js
@@ -1,36 +1,58 @@
-const bcrypt = require('bcrypt');
-let {customers} = require('./customers');
-let {quizzes} = require('./data')
+const bcrypt = require("bcrypt");
+let { customers } = require("./customers");
+let { quizzes } = require("./data");
+let { flowers } = require("./flowers");
+let { score } = require("./scores");
 
 let store = {
-
-    addCustomer: (name, email, password) => {
-        const hash = bcrypt.hashSync(password, 10);
-        customers.push({id: 1, name: name, email: email, password: hash});
-    },
-
-    login: (email, password) => {
-        let customer = customers.find(x => x.email.toLowerCase() === email.toLowerCase());
-        if(customer){
-            let valid = bcrypt.compareSync(password, customer.password);
-            if (valid) {
-                return {valid: true};
-            } else {
-                return {valid: false, message: 'Credentials are not valid.'}
-            }
-        } else {
-            return {valid: false, message: 'Email not found.'};
-        }
-    },
-
-    getQuiz: (id) => {
-        let quiz = quizzes.find(x = x.name.toLowerCase() === id.toLowerCase());
-        if (quiz) {
-            return {done: true, quiz};
-        } else {
-            return {done: false, message: 'No quiz with this game was found.'
-        }
-        }
+  addCustomer: (name, email, password) => {
+    const hash = bcrypt.hashSync(password, 10);
+    if (customers.includes(email)) {
+      return { done: false };
+    } else {
+      customers.push({ id: 1, name: name, email: email, password: hash });
+      return { done: true };
     }
-}
-module.exports = {store}
+  },
+
+  login: (email, password) => {
+    let customer = customers.find(
+      (x) => x.email.toLowerCase() === email.toLowerCase()
+    );
+    if (customer) {
+      let valid = bcrypt.compareSync(password, customer.password);
+      if (valid) {
+        return { valid: true };
+      } else {
+        return { valid: false, message: "Credentials are not valid." };
+      }
+    } else {
+      return { valid: false, message: "Email not found." };
+    }
+  },
+
+  getQuiz: (id) => {
+    let quiz = quizzes.find((x) => x.name.toLowerCase() === id.toLowerCase());
+    if (quiz) {
+      return { done: true, quiz };
+    } else {
+      return { done: false, message: "No quiz with this game was found." };
+    }
+  },
+
+  getFlower: () => {
+    let flowerlist = flowers;
+    return { done: true, flowerlist };
+  },
+  score: (quizTaker, quizName, score, date) => {
+    score.push({
+      quizTaker: quizTaker,
+      quizName: quizName,
+      score: score,
+      date: date,
+    });
+    return { done: true, message: "Score is saved" };
+  },
+};
+
+module.exports = { store };
