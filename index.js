390f269a62be72fed8b868aaf7dcda8dd3abd09b
Thu <thutra@email.arizona.edu>
2022-04-27 11:08:55 -0700

    change config

--- index.js
+++ index.js
@@ -13,11 +13,13 @@ const port = process.env.PORT || 4002;
 
 //middlewares
 application.use(express.json());
-application.use(cors({
+app.options('*', cors());
+application.use(cors(
+  //{
   //origin: 'http://localhost:3000',
-  origin: 'https://nganph5.github.io/',
-  credentials: true
-}));
+  // origin: 'https://nganph5.github.io/',
+  // credentials: true}
+  ));
 
 passport.use(new LocalStrategy({ usernameField: 'email'}, function verify(username, password, cb) {
   store.login(username, password)
